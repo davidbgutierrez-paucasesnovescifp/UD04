1dc32874cad0e1fb98025aa4bb7c6cc6e3116650
David Gutiérrez Cladera <David Gutirrez Cladera>
2025-02-16 00:54:39 +0100

    Se añaden comentarios Javadoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -8,10 +8,22 @@ public class CCuenta {
     private double saldo;
     private double tipoInteres;
 
+    /**
+     * Inicializar clase sin parámetros
+     * @author David Gutiérrez
+     */
     public CCuenta()
     {
     }
 
+    /**
+     * Inicializar clase con todas las propiedades
+     * @param nom Nombre del propietario de la cuenta.
+     * @param cue Número de la cuenta.
+     * @param sal Salario de la cuenta.
+     * @param tipo Tipo de interés
+     * @author David Gutiérrez
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -19,50 +31,87 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Obtiene el nombre del propietario de la cuenta
+     * @return Nombre del propietario de la cuenta
+     * @author David Gutiérrez
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Establece el nombre del propietario de la cuenta
+     * @param nombre Nombre del propietario de la cuenta
+     * @author David Gutiérrez
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene el número de la cuenta
+     * @return Número de la cuenta
+     * @author David Gutiérrez
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     * Establece el número de la cuenta
+     * @param cuenta Número de la cuenta
+     * @author David Gutiérrez
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
-    }
-
+    /**
+     * Obtiene el tipo de interés de la cuenta.
+     * @return Tipo de interés
+     * @author David Gutiérrez
+     */
     public double getTipoInteres() {
         return tipoInteres;
     }
 
+    /**
+     * Establece el tipo de interés de la cuenta.
+     * @param tipoInteres El tipo de interés.
+     * @author David Gutiérrez
+     */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
 
+    /**
+     * Obtiene la cantidad de saldo actual de la cuenta.
+     * @return saldo
+     * @author David Gutiérrez
+     */
     public double estado()
     {
         return saldo;
     }
-
+    /**
+     * Incrementa el saldo de la cuenta.
+     * @param cantidad Cantidad a ingresar.
+     * @throws Exception La cantidad es negativa
+     * @author David Gutiérrez
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = saldo + cantidad;
     }
-
+    /**
+     * Retira el saldo de la cuenta.
+     * @param cantidad Cantidad a retirar.
+     * @throws Exception La cantidad es negativa o no hay suficiente saldo para retirar
+     * @author David Gutiérrez
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
