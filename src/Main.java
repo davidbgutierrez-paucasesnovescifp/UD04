e27745c6e15aa327ae2215d528a0283891d8f9d3
David Gutiérrez Cladera <David Gutirrez Cladera>
2025-02-16 00:17:03 +0100

    Primer commit y push.

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import cuentas.CCuenta;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        operativa_cuenta(cuenta1,100);
+    }
+
+    public static void operativa_cuenta(CCuenta cuenta,float cantidad) {
+        double saldoActual = cuenta.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+
+
+        try {
+            cuenta.retirar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
